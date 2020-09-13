b21c5131f3b3eec1825b4594ee7b514914bfd35a
Gabriel Torres <gtorress106@gmail.com>
2020-09-12 21:00:05 -0600

    Produccion

---
+++ src/app/pages/item/item.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { ProductoDescripcion } from 'src/app/interfaces/producto-Descripcion.interface';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+
+  producto: ProductoDescripcion;
+  id: string;
+
+  constructor( private route: ActivatedRoute, public productoService : ProductosService) { }
+
+  ngOnInit(): void {
+
+    this.route.params.subscribe( parametros => {
+      console.log(parametros);
+      this.productoService.getProducto(parametros['id'])
+      .subscribe( (producto: ProductoDescripcion) => {
+         this.producto = producto;
+         this.id = parametros['id'];
+      });
+    });
+  }
+
+}
